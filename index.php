afc78bd7d1d2e94e5dd308ac52dbdc3ac51477c6
helliot01 <80601617+helliot01@users.noreply.github.com>
2021-10-15 13:12:32 -0400

    Update index.php

--- index.php
+++ index.php
@@ -15,19 +15,52 @@
     </style>
 
     <body>
+        
 
+           
         <div class="container">
             <h1 style="text-align:center"> CS460 PA 1.2</h1><br>
             <h3 style="text-align:center"> Movie Madness </h3><br>
+            <p style="text-align:center"> Created By Harrison Elliot and Tabitha Oanda </p><br>
         </div>
 
-        
+
+       <!-- Working on adding different viewing function
+
+        <form action="/action_page.php">
+        <p>View All:</p>
+          <input type="radio" id="movies" name="fav_language" value="Movies">
+          <label for="movies">Movies</label><br>
+          <input type="radio" id="series" name="fav_language" value="Series">
+          <label for="series">Series</label><br>
+          <input type="radio" id="people" name="fav_language" value="People">
+          <label for="people">People</label>
+        <br>
+        <input type="submit" value="View">
+        </form>
+
+        -->
+
         <div class = "container">
+            <button id="regClicked">Register Here</button>
+            <form id="registrationForm" method="post" action="">
+                <div id="regForm" class="input-group mb-3" style="display:none">
+                    <input type="text" class="form-control" placeholder="Enter User Email" name="regUID" id="regUID">
+                    <input type="text" class="form-control" placeholder="Enter Name" name="regName" id="regName">
+                    <input type="text" class="form-control" placeholder="Enter Age" name="regAge" id="regAge">
+                    <button class="btn btn-outline-secondary" type="submit" name="reg_submitted" id="button-addon2">Register</button>
+                </div>
+            </form>
+
+            <br>
+
             <form method= "post">
 
                 <input type="submit" name="button1" value="All Movies"/>
                 <input type="submit" name="button2" value="All Series"/>
-                <input type="submit" name="button3" value="All People"/>
+                <input type="submit" name="button3" value="All People"/>                
+                <input type="submit" name="button5" value="All Actors"/>
+                <input type="submit" name="button4" value="All Users"/>
             </form>
         </div>
 
@@ -72,9 +105,126 @@
             </form>
         </div>
 
+        <script>
+            $(function(){
+                $('#regClicked').on('click',function(){  
+                    $('#regForm').show();
+                    });
+                });
+        </script>
 
         <div class="container">
             <?php
+
+
+                if(isset($_POST['button5']))
+                {
+
+                    echo "<table class='table table-md table-bordered'>";
+                    echo "<thead class='thead-dark' style='text-align: center'>";
+                    echo "<tr><th class='col-md-2'>Name</th><th class='col-md-2'>Motion Picture ID</th></tr></thead>";
+                    
+                    class TableRows extends RecursiveIteratorIterator {
+                        function __construct($it) {
+                            parent::__construct($it, self::LEAVES_ONLY);
+                        }
+
+                        function current() {
+                            // return "<td style='width: 30px; border: 1px solid black;'>" . parent::current(). "</td>";
+                            return "<td style='text-align:center'>" . parent::current(). "</td>";
+                        }
+
+                        function beginChildren() {
+                            echo "<tr>";
+                        }
+
+                        function endChildren() {
+                            echo "</tr>" . "\n";
+                        }
+                }
+
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $dbname = "pa1.2";
+
+                try {
+                    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                    // SQL
+                    $stmt = $conn->prepare("SELECT name, mpid FROM Role INNER JOIN People where role.pid=people.id and role.role_name='Actor'");
+                    $stmt->execute();
+
+                    // set the resulting array to associative
+                    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+                        echo $v;
+                    }
+                }
+
+                catch(PDOException $e) {
+                    echo "Error: " . $e->getMessage();
+                }
+                $conn = null;
+                echo "</table>";
+                }
+
+
+                if(isset($_POST['button4']))
+                {
+
+                    echo "<table class='table table-md table-bordered'>";
+                    echo "<thead class='thead-dark' style='text-align: center'>";
+                    echo "<tr><th class='col-md-2'>Name</th><th class='col-md-2'>User Email</th><th class='col-md-2'>Age</th></tr></thead>";
+                    
+                    class TableRows extends RecursiveIteratorIterator {
+                        function __construct($it) {
+                            parent::__construct($it, self::LEAVES_ONLY);
+                        }
+
+                        function current() {
+                            // return "<td style='width: 30px; border: 1px solid black;'>" . parent::current(). "</td>";
+                            return "<td style='text-align:center'>" . parent::current(). "</td>";
+                        }
+
+                        function beginChildren() {
+                            echo "<tr>";
+                        }
+
+                        function endChildren() {
+                            echo "</tr>" . "\n";
+                        }
+                }
+
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $dbname = "pa1.2";
+
+                try {
+                    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                    // SQL
+                    $stmt = $conn->prepare("SELECT name, uemail, age FROM User");
+                    $stmt->execute();
+
+                    // set the resulting array to associative
+                    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+                        echo $v;
+                    }
+                }
+
+                catch(PDOException $e) {
+                    echo "Error: " . $e->getMessage();
+                }
+                $conn = null;
+                echo "</table>";
+            }
+            
+
                 if(isset($_POST['button3']))
                 {
 
@@ -133,7 +283,7 @@
 
                     echo "<table class='table table-md table-bordered'>";
                     echo "<thead class='thead-dark' style='text-align: center'>";
-                    echo "<tr><th class='col-md-2'>Name</th><th class='col-md-2'>Production</th><th class='col-md-2'>Budget</th><th class='col-md-2'>Rating</th><th class='col-md-2'># of Seasons</th></tr></thead>";
+                    echo "<tr><th class='col-md-2'>Name</th><th class='col-md-2'>Production</th><th class='col-md-2'>Budget</th><th class='col-md-2'>Rating</th><th class='col-md-2'># of Seasons</th><th class='col-md-2'>ID</th></tr></thead>";
                     
                     class TableRows extends RecursiveIteratorIterator {
                         function __construct($it) {
@@ -154,6 +304,30 @@
                         }
                 }
 
+                    //Adding Functionality To Like Movies / Series
+                    class LikeRows extends RecursiveIteratorIterator
+                    {
+                        function __construct($it)
+                        {
+                            parent::__construct($it, self::LEAVES_ONLY);
+                        }
+            
+                        function current()
+                        {
+                            return "<td style='text-align:center'>" . parent::current() ."</td>";
+                        }
+            
+                        function beginChildren()
+                        {
+                            echo "<tr>";
+                        }
+            
+                        function endChildren() {
+                            echo '<td><input type="button" name = "like_button" value = "Like"/></td>';
+                            echo "</tr>" . "\n";
+                        } 
+                }
+
                 $servername = "localhost";
                 $username = "root";
                 $password = "";
@@ -164,12 +338,12 @@
                     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
                     // SQL
-                    $stmt = $conn->prepare("SELECT name, production, budget, rating, season_count FROM series INNER JOIN motion_picture on series.mpid=motion_picture.id");
+                    $stmt = $conn->prepare("SELECT name, production, budget, rating, season_count, id FROM series INNER JOIN motion_picture on series.mpid=motion_picture.id");
                     $stmt->execute();
 
                     // set the resulting array to associative
                     $stmt->setFetchMode(PDO::FETCH_ASSOC);
-                    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+                    foreach(new LikeRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
                         echo $v;
                     }
                 }
@@ -186,7 +360,7 @@
 
                     echo "<table class='table table-md table-bordered'>";
                     echo "<thead class='thead-dark' style='text-align: center'>";
-                    echo "<tr><th class='col-md-2'>Name</th><th class='col-md-2'>Production</th><th class='col-md-2'>Budget</th><th class='col-md-2'>Rating</th><th class='col-md-2'>Box Office Collection</th></tr></thead>";
+                    echo "<tr><th class='col-md-2'>Name</th><th class='col-md-2'>Production</th><th class='col-md-2'>Budget</th><th class='col-md-2'>Rating</th><th class='col-md-2'>Box Office Collection</th><th class='col-md-2'>ID</th></tr></thead>";
                     
                     class TableRows extends RecursiveIteratorIterator {
                         function __construct($it) {
@@ -206,6 +380,28 @@
                             echo "</tr>" . "\n";
                         }
                 }
+                    class LikeRows extends RecursiveIteratorIterator
+                    {
+                        function __construct($it)
+                        {
+                            parent::__construct($it, self::LEAVES_ONLY);
+                        }
+            
+                        function current()
+                        {
+                            return "<td style='text-align:center'>" . parent::current() ."</td>";
+                        }
+            
+                        function beginChildren()
+                        {
+                            echo "<tr>";
+                        }
+            
+                        function endChildren() {
+                            echo '<td><input type="button" name = "like_button" value = "Like"/></td>';
+                            echo "</tr>" . "\n";
+                        } 
+                }
 
                 $servername = "localhost";
                 $username = "root";
@@ -217,12 +413,12 @@
                     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
                     // SQL
-                    $stmt = $conn->prepare("SELECT name, production, budget, rating, boxoffice_collection FROM Movie INNER JOIN motion_picture on Movie.mpid=motion_picture.id");
+                    $stmt = $conn->prepare("SELECT name, production, budget, rating, boxoffice_collection, id FROM Movie INNER JOIN motion_picture on Movie.mpid=motion_picture.id");
                     $stmt->execute();
 
                     // set the resulting array to associative
                     $stmt->setFetchMode(PDO::FETCH_ASSOC);
-                    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+                    foreach(new LikeRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
                         echo $v;
                     }
                 }
@@ -296,6 +492,10 @@
                 {
                     $userID = $_POST["userID"]; 
 
+                    $friendlyMessage = "Showing " . $userID . "'s likes:";
+
+                    echo $friendlyMessage;
+
                     echo "<table class='table table-md table-bordered'>";
                     echo "<thead class='thead-dark' style='text-align: center'>";
                     echo "<tr><th class='col-md-2'>Name</th><th class='col-md-2'>Rating</th></tr></thead>";
@@ -354,7 +554,6 @@
                     $newLikeUID = $_POST["newLikeUID"]; 
                     $newLikeMPID = $_POST["newLikeMPID"]; 
 
-                    echo $newLikeUID;
 
                     echo "<table class='table table-md table-bordered'>";
                     echo "<thead class='thead-dark' style='text-align: center'>";
@@ -389,7 +588,6 @@
                         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
                         // SQL
-                        //$stmt = $conn->prepare("INSERT INTO motion_picture (id, name, production, budget, rating) VALUES (2, 'Indiana Jones', 'Miramax, 50000, 6.7)");
                         $stmt = $conn->prepare("INSERT INTO Likes (uemail, mpid) VALUES ('$newLikeUID', $newLikeMPID)");
                         $stmt->execute();
 
@@ -411,6 +609,71 @@
                 {
                     $userID = 0;
                 }
+
+
+                if(isset($_POST['reg_submitted']))
+                {
+                    $regUID = $_POST["regUID"]; 
+                    $regName = $_POST["regName"];
+                    $regAge = $_POST["regAge"]; 
+ 
+
+
+                    echo "<table class='table table-md table-bordered'>";
+                    echo "<thead class='thead-dark' style='text-align: center'>";
+                    echo "<tr><th class='col-md-2'>User</th><th class='col-md-2'>Name</th></tr></thead>";
+
+                    class TableRows extends RecursiveIteratorIterator {
+                        function __construct($it) {
+                            parent::__construct($it, self::LEAVES_ONLY);
+                        }
+
+                        function current() {
+                            // return "<td style='width: 30px; border: 1px solid black;'>" . parent::current(). "</td>";
+                            return "<td style='text-align:center'>" . parent::current(). "</td>";
+                        }
+
+                        function beginChildren() {
+                            echo "<tr>";
+                        }
+
+                        function endChildren() {
+                            echo "</tr>" . "\n";
+                        }
+                    }
+
+                    $servername = "localhost";
+                    $username = "root";
+                    $password = "";
+                    $dbname = "pa1.2";
+
+                    try {
+                        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                        // SQL
+                        $stmt = $conn->prepare("INSERT INTO User (uemail, name, age) VALUES ('$regUID', '$regName', $regAge)");
+                        $stmt->execute();
+
+
+
+                        // set the resulting array to associative
+                        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                        foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+                            echo $v;
+                        }
+                    }
+                    catch(PDOException $e) {
+                        echo "Error: " . $e->getMessage();
+                    }
+                    $conn = null;
+                    echo "</table>";
+                }
+                else
+                {
+                    $userID = 0;
+                }
+
 
             ?>
         </div>
